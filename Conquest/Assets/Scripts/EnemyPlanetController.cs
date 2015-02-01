1d45c5953c97490c9f5fb7bebfd4f34643dfa88c
Jason Patton <jasmpatton@gmail.com>
2015-02-01 15:34:34 -0700

    Added the game files we currently have.

---
+++ Conquest/Assets/Scripts/EnemyPlanetController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyPlanetController : MonoBehaviour {
+	
+	public float productionRate;
+	public float ships;
+	private GUIText text;
+	public string owner;
+	public GameObject ship;
+	public Transform shipSpawn;
+	public GameController gameController;
+	// Use this for initialization
+	void Start () {
+		text = GetComponentInChildren<GUIText>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ships += Time.deltaTime * productionRate;
+		text.text = ((int)ships).ToString();
+		
+		if (Input.GetButton ("Fire1") && ships >= 1) 
+		{
+			ships--;
+			Instantiate (ship, shipSpawn.position, shipSpawn.rotation);
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		//ignore boundary
+		if (other.tag == "Boundary") 
+		{
+			return;
+		}
+		
+		if (other.tag == "Ship")
+		{
+			//if its your own ship
+			if (owner == other.gameObject.name.ToString())
+			{
+				//Debug.Log(owner);
+				ships++;
+			}
+			//decrement your ships
+			else if ((int)ships >= 1)
+			{
+				ships--;
+			}
+			else
+			{
+				gameController.EndGame("You defeated the Enemy!");
+				//ships = 1f;
+				//string substring = other.gameObject.name.ToString ();
+				////owner = other.gameObject.name.ToString();
+				//owner = System.Text.RegularExpressions.Regex.Replace (substring, "(Clone)", "");
+			}
+			
+			Destroy (other.gameObject);//destroy the ship
+		}
+	}//end OnTriggerEnter
+	
+	
+}
---
+++ Conquest/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+
+	public Text gameOverText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void EndGame(string message)
+	{
+		gameOverText.text = message;
+	}
+}
