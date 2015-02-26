f5edc531972b2d53c58ad1a7a1a2ad49084dbacf
jmpatton <jasmpatton@gmail.com>
2015-02-26 10:58:24 -0700

    Added more to the GameController
    Made it so it would check whether the game is won (all planets are
    player) or lost (no planets are player).  Uses a button to let the
    player advance to the next level if won, or restart the level if lost.
    Also displays "You Won!" or "You lost..."

--- Conquest/Assets/Scripts/GameController.cs
+++ Conquest/Assets/Scripts/GameController.cs
@@ -1,19 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-
+    public List<GameObject> planets = new List<GameObject>();
 	public Text gameOverText;
-
+    public Button restartButton;
+    public Text restartText;
+    private int count;
 	// Use this for initialization
 	void Start () {
-	
+        planets.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+        planets.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        planets.AddRange(GameObject.FindGameObjectsWithTag("Neutral"));
+        gameOverText.text = "";
+        restartButton.enabled = false;
+        restartButton.GetComponent<CanvasRenderer>().SetAlpha(0);
+        restartText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        count = 0;
+        foreach (GameObject i in planets)
+        {
+            if (i.tag == "Player")
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            EndGame("You lost...");
+            restartButton.enabled = true;
+            restartButton.GetComponent<CanvasRenderer>().SetAlpha(1);
+            restartText.text = "Restart Level";
+        }
+        if (count == planets.Count)
+        {
+            restartButton.enabled = true;
+            restartButton.GetComponent<CanvasRenderer>().SetAlpha(1);
+            restartText.text = "Next Level";
+            restartButton.onClick.RemoveAllListeners();
+            restartButton.onClick.AddListener(() => { NextLevel(); });
+            EndGame("You Win!");
+        }
 
 	}
 
@@ -21,4 +54,14 @@ public class GameController : MonoBehaviour {
 	{
 		gameOverText.text = message;
 	}
+
+    public void RestartLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    void NextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
 }
